6f21c16d8aee29d71ca581a918204ea0feae8426
chris <chris@debian>
2017-05-22 22:28:37 -0500

    source profile

--- install.sh
+++ install.sh
@@ -60,3 +60,4 @@ echo "stty -ixon  # prevents Ctrl-S freezes in putty" >> ~/.bashrc
 echo 'PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:[\[\033[33;1m\]\w\[\033[32m\]] \[\033[1;33m\]-> \[\033[0m\]"' >> ~/.bashrc
 
 
+source ~/.profile
