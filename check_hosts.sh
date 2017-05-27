18237e8e6d9c5cd9b1168a553507f8cf2bfb4924
chris <chris@debian>
2017-05-27 04:03:49 -0500

    other stuff

--- check_hosts.sh
+++
@@ -1,1 +0,0 @@
-s host1,host2,host3 ./bin/system_summary | mail -s 'systems summary' user@hostname
---
+++ postgresql_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install postgresql
+
+read -p "add 'host   all    all   192.168.xxx.0/24  md5' to /etc/postgresql/x.x/main/pg_hba.conf" 
+read -p "add 'local_address='*'' to /etc/postgresql/x.x/main/postgresql.conf" 
+
+# psql -h <host> -U <user>
+# \l                list databases
+# \c                connect to database
+# \d{t|i|s|v|S}     list tables/indexes/sequences/views/system tables
