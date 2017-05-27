dac1c786ca324596afdefb7a48241d51d41633e0
chris <chris@debian>
2017-05-27 04:13:19 -0500

    clarify

--- postgresql_setup.sh
+++ postgresql_setup.sh
@@ -6,7 +6,7 @@ sudo apt-get install postgresql
 read -p "add 'host   all    all   192.168.xxx.0/24  md5' to /etc/postgresql/x.x/main/pg_hba.conf" 
 read -p "add 'local_address='*'' to /etc/postgresql/x.x/main/postgresql.conf" 
 
-# psql -h <host> -U <user>
+# psql -h <host> -U <user> -d <database>
 # \l                list databases
 # \c                connect to database
 # \d{t|i|s|v|S}     list tables/indexes/sequences/views/system tables
